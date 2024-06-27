123ae33a767b71dfdf88135e7f6a4365fc3d9499
itbromindo <itbromindo@gmail.com>
2024-06-27 13:31:30 +0700

    fix export pembayaran

--- app/Exports/ReceivablePaymentExport.php
+++ app/Exports/ReceivablePaymentExport.php
@@ -4,15 +4,16 @@ namespace App\Exports;
 
 use App\Models\ReceivablePayment;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ReceivablePaymentExport implements FromCollection
+class ReceivablePaymentExport implements FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        return ReceivablePayment::join('sales_orders as so', 'so.invoice_number', '=', 'rp.invoice_number')->join('customer c', 'c.uid', '=', 'so.uid_customer')->join('payment_method as pm', 'pm.uid', '=', 'rp.uid_payment_method')->select('rp.invoice_number', 'c.name as customer_name', 'rp.transaction_date', 'rp.amount', 'rp.term', 'pm.name as payment_method')->where('rp.status', 1)->orderBy('rp.uid', 'desc')->get();
+        return ReceivablePayment::join('sales_orders as so', 'so.invoice_number', '=', 'receivable_payments.invoice_number')->join('customer as c', 'c.uid', '=', 'so.uid_customer')->join('payment_method as pm', 'pm.uid', '=', 'receivable_payments.uid_payment_method')->select('receivable_payments.invoice_number', 'c.name as customer_name', 'receivable_payments.transaction_date', 'receivable_payments.amount', 'receivable_payments.term', 'pm.name as payment_method')->where('receivable_payments.status', 1)->orderBy('receivable_payments.uid', 'desc')->get();
 
     }
 

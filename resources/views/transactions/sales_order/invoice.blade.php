8520e1564c9914ed211054bf104b256fd08422fb
itbromindo <itbromindo@gmail.com>
2024-08-08 12:09:35 +0700

    font size and font weight pada pdf

--- resources/views/transactions/sales_order/invoice.blade.php
+++ resources/views/transactions/sales_order/invoice.blade.php
@@ -40,12 +40,13 @@ function integers($angka)
         }
 
         table {
-            font-size: x-small;
+            font-size: small;
+            font-weight: bold;
         }
 
         tfoot tr td {
             font-weight: bold;
-            font-size: x-small;
+            font-size: small;
         }
 
         .gray {
@@ -163,7 +164,7 @@ function integers($angka)
             @endphp
             @foreach ($data['detail'] as $d)
                         <?php 
-                                                          if ($d->length != 0 || $d->width != 0) {
+                                                                      if ($d->length != 0 || $d->width != 0) {
                     $size = $d->length . 'x' . $d->width;
 
                     $subtotal = ($d->length * $d->width * $d->qty * $d->price / 10000);
@@ -175,7 +176,7 @@ function integers($angka)
 
                 }
                 $grand_total += $subtotal;
-                                                                                                                                                                        ?>
+                                                                                                                                                                                    ?>
 
                         <tr>
                             <td scope="row">{{ $no++ }}</td>

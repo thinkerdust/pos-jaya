a5093fea1e0c2e04babc62d0adb9d1c1c14f8a5c
itbromindo <itbromindo@gmail.com>
2024-08-15 00:27:35 +0700

    fix bug company

--- app/Exports/ReceivablePaymentExport.php
+++ app/Exports/ReceivablePaymentExport.php
@@ -13,12 +13,15 @@ class ReceivablePaymentExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        return ReceivablePayment::join('sales_orders as so', 'so.invoice_number', '=', 'receivable_payments.invoice_number')->join('customer as c', 'c.uid', '=', 'so.uid_customer')->join('payment_method as pm', 'pm.uid', '=', 'receivable_payments.uid_payment_method')->select('receivable_payments.invoice_number', 'c.name as customer_name', 'receivable_payments.transaction_date', 'receivable_payments.amount', 'receivable_payments.term', 'pm.name as payment_method')->where('receivable_payments.status', 1)->orderBy('receivable_payments.uid', 'desc')->get();
+        return ReceivablePayment::leftJoin('sales_orders as so', function ($join) {
+            $join->on('receivable_payments.invoice_number', '=', 'so.invoice_number');
+            $join->on('receivable_payments.uid_company', '=', 'so.uid_company');
+        })->join('customer as c', 'c.uid', '=', 'so.uid_customer')->join('payment_method as pm', 'pm.uid', '=', 'receivable_payments.uid_payment_method')->join('company as c','receivable_payments.uid_company','=','c.uid')->select('receivable_payments.invoice_number', 'c.name as customer_name', 'receivable_payments.transaction_date', 'receivable_payments.amount', 'receivable_payments.term', 'pm.name as payment_method','c.name as company')->where('receivable_payments.status', 1)->orderBy('receivable_payments.uid', 'desc')->get();
 
     }
 
     public function headings(): array
     {
-        return ["NO INVOICE", "CUSTOMER", "TANGGAL", "JUMLAH", "TERMIN", "METODE PEMBAYARAN"];
+        return ["NO INVOICE", "CUSTOMER", "TANGGAL", "JUMLAH", "TERMIN", "METODE PEMBAYARAN","COMPANY"];
     }
 }
